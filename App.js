9c1156554cc53f6e75082402e38b8b73de2475cb
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-03 13:31:49 -0700

    feature: added tab navigation

--- App.js
+++ App.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import History from './components/History';
+import { StyleSheet, View, Platform } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+import History from './components/History';
+import AddEntry from './components/AddEntry';
 import reducer from './reducers';
+import { purple, white } from './utils/colors';
+
+const RouteConfig = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+      ),
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="plus-square" size={30} color={tintColor} />
+      ),
+    },
+  },
+};
+const TabNavConfig = {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+};
+
+const Tab =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={styles.container}>
-        <History />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={History} />
+            <Tab.Screen name="Settings" component={AddEntry} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   );
