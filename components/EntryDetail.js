9fb6291427e6ebf617fc40f4bd5161721925b01d
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-03 20:33:06 -0700

    feature: Entry detail now has date as the title

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 
-class EntryDetail extends Component {
-  render() {
-    return (
-      <View>
-        <Text>
-          Entry Detail -{JSON.stringify(this.props.route.params.entryId)}
-        </Text>
-      </View>
-    );
-  }
-}
+const EntryDetail = ({ navigation, route }) => {
+  const { entryId } = route.params;
+
+  const year = entryId.slice(0, 4);
+  const month = entryId.slice(5, 7);
+  const day = entryId.slice(8);
+
+  navigation.setOptions({ title: `${month}/${day}/${year}` });
+
+  return (
+    <View>
+      <Text>Entry Detail -{JSON.stringify(entryId)}</Text>
+    </View>
+  );
+};
 
 export default EntryDetail;
